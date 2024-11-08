f700a5de990cfd509218a3875f66ffbabb56b7cf
Nelson Eljadue <88507546+Eljaduen@users.noreply.github.com>
2024-11-08 16:09:33 -0500

    cambio de transision>

--- src/services/viewTransitions.js
+++ src/services/viewTransitions.js
@@ -1,4 +1,4 @@
-document?.addEventListener('astro:page-load', ()=> {
+document.addEventListener('astro:page-load', ()=> {
     for (const element of document.querySelectorAll('#fade-image')) {
         element.classList.remove('scale-90')
     }
